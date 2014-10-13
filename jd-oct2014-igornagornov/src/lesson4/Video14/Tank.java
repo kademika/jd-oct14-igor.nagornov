173dca5f32d03d49b07e90b3e9bf2dd459d14cf5
Igor Nagornov <shadow-in88@mail.ru>
2014-10-13 15:24:02 +0400

    I've added video14 and video15

--- jd-oct2014-igornagornov/src/lesson4/Video14/Tank.java
+++ jd-oct2014-igornagornov/src/lesson4/Video14/Tank.java
@@ -6,4 +6,14 @@ public class Tank {
 	int crew;
 	int maxSpeed;	
 	
+	Tank(){
+		
+	}
+	
+	Tank(String color, int crew, int maxSpeed){
+		this.color = color;
+		this.crew = crew;
+		this.maxSpeed = maxSpeed;
+	}
+	
 }
--- jd-oct2014-igornagornov/src/lesson4/Video14/printTankInfo.java
+++ jd-oct2014-igornagornov/src/lesson4/Video14/printTankInfo.java
@@ -16,6 +16,10 @@ public class printTankInfo {
 			
 			System.out.println("Мы создали " + t.color + " танк" + ", экипаж из " + t.crew + " человек," +
 						" максимальная скорость - " + t.maxSpeed);
+			
+			Tank t2 = new Tank("cерый", 5, 150);
+			System.out.println("Мы создали " + t2.color + " танк" + ", экипаж из " + t2.crew + " человек," +
+					" максимальная скорость - " + t2.maxSpeed);
 		}
 
 }
