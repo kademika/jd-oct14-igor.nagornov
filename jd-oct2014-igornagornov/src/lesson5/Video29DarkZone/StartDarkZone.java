6c56892c1c5aaad6fa77126d552e5cd28591dc1b
Igor Nagornov <shadow-in88@mail.ru>
2014-10-24 15:34:34 +0400

    I've added DarkZone

---
+++ jd-oct2014-igornagornov/src/lesson5/Video29DarkZone/StartDarkZone.java
@@ -0,0 +1,12 @@
+package lesson5.Video29DarkZone;
+
+public class StartDarkZone {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub		
+		
+		DarkZone.darkZone(1000, 12, 1000000);
+
+	}
+
+}
