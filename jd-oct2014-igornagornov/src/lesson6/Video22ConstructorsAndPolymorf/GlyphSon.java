d18b9c034629cc8ad73998a5cb5005d013a1d2c6
Igor Nagornov <shadow-in88@mail.ru>
2014-11-05 16:43:35 +0400

    Changes in video22

---
+++ jd-oct2014-igornagornov/src/lesson6/Video22ConstructorsAndPolymorf/GlyphSon.java
@@ -0,0 +1,15 @@
+package lesson6.Video22ConstructorsAndPolymorf;
+
+public class GlyphSon extends Glyph{
+	
+	private String name = "GlyphSon";
+	
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+		
+		System.out.println("GlyphSon is drowing");
+		name.length();
+	}
+	
+}
