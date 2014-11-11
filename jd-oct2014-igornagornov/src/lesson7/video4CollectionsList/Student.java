8a24eb03e9086d9c242cdbf1b9329aaae02114f0
Igor Nagornov <shadow-in88@mail.ru>
2014-11-11 17:16:14 +0400

    I've added ArrayList example

---
+++ jd-oct2014-igornagornov/src/lesson7/video4CollectionsList/Student.java
@@ -0,0 +1,22 @@
+package lesson7.video4CollectionsList;
+
+public class Student {
+	
+	private String name;
+	private String secondName;	
+	
+	public Student(String name, String  secondName) {
+		// TODO Auto-generated constructor stub
+		this.name = name;
+		this.secondName = secondName;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public String getSecondName() {
+		return secondName;
+	}
+	
+
+}
